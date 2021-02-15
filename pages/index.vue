4069deee6ec42e0d3e18c8147373524042082f3d
Nick Barry <itsnickbarry@protonmail.ch>
2021-02-15 13:47:54 -0600

    rename CSS classes and assets

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,27 @@
 <template>
   <body>
-    <div class="v1_2">
-      <div class="v3_26" />
-      <div class="v3_28" />
+    <div class="container">
+      <div class="space" />
+      <div class="car-red" />
 
-      <span class="v3_22">Get to the Moon
-        with your sTSLA</span>
-      <div class="v3_23" />
+      <span class="title">Get to the Moon
+        with your sTSLA
+      </span>
 
-      <span class="v3_24">Get sTSLA</span>
+      <div class="button" />
 
-      <span class="v3_25">Synthetix is the backbone for derivatives
+      <span class="button-text">Get sTSLA</span>
+
+      <span class="paragraph">Synthetix is the backbone for derivatives
         trading in DeFi, allowing anyone, anywhere
         to gain on-chain exposure to a vast range
         of assets.
       </span>
 
-      <div class="v3_27" />
-      <div class="v14_3" />
-      <div class="v14_4" />
-      <div class="v14_5" />
+      <div class="car-gray" />
+      <div class="github-icon" />
+      <div class="twitter-icon" />
+      <div class="network-icon" />
     </div>
   </body>
 </template>
@@ -30,7 +32,7 @@
   padding: 0px !important;
   margin: 0px !important;
 }
-.v1_2 {
+.container {
   width: 100%;
   height: 1024px;
   background: linear-gradient(rgba(0,2,75,1), rgba(39,30,110,1));
@@ -40,10 +42,10 @@
   left: 0px;
   overflow: hidden;
 }
-.v3_26 {
+.space {
   width: 100%;
   height: 100%;
-  background: url("../public/v3_26.png");
+  background: url("../public/space.png");
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
@@ -53,10 +55,10 @@
   left: 162px;
   overflow: hidden;
 }
-.v3_28 {
+.car-red {
   width: 318px;
   height: 155px;
-  background: url("../public/v3_28.png");
+  background: url("../public/car-red.png");
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
@@ -67,7 +69,7 @@
   transform: rotate(-32deg);
   overflow: hidden;
 }
-.v3_22 {
+.title {
   width: 470px;
   color: rgba(255,255,255,1);
   position: absolute;
@@ -79,7 +81,7 @@
   opacity: 1;
   text-align: left;
 }
-.v3_23 {
+.button {
   width: 186px;
   height: 48px;
   background: linear-gradient(rgba(130,68,176,1), rgba(255,0,119,1));
@@ -93,7 +95,7 @@
   border-bottom-right-radius: 50px;
   overflow: hidden;
 }
-.v3_24 {
+.button-text {
   width: 125px;
   color: rgba(255,255,255,1);
   position: absolute;
@@ -101,11 +103,11 @@
   left: 101px;
   font-family: Montserrat;
   font-weight: SemiBold;
-  font-size: 24px;
+  font-size: 20px;
   opacity: 1;
   text-align: left;
 }
-.v3_25 {
+.paragraph {
   width: 519px;
   color: rgba(147,159,189,1);
   position: absolute;
@@ -117,10 +119,10 @@
   opacity: 1;
   text-align: left;
 }
-.v3_27 {
+.car-gray {
   width: 523px;
   height: 297px;
-  background: url("../public/v3_27.png");
+  background: url("../public/car-gray.png");
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
@@ -130,7 +132,7 @@
   left: 331px;
   overflow: hidden;
 }
-.v14_3 {
+.github-icon {
   width: 38px;
   height: 37px;
   background: rgba(255,255,255,1);
@@ -139,7 +141,7 @@
   top: 809px;
   left: 70px;
 }
-.v14_4 {
+.twitter-icon {
   width: 41px;
   height: 34px;
   background: rgba(255,255,255,1);
@@ -148,7 +150,7 @@
   top: 873px;
   left: 70px;
 }
-.v14_5 {
+.network-icon {
   width: 23px;
   height: 36px;
   background: rgba(255,255,255,1);
