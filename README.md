dbdf033069a844f6dff37ff0ab1b28b450ce38c0
Nick Barry <itsnickbarry@protonmail.ch>
2021-02-15 13:41:12 -0600

    setup nuxt app, import design from Figma

---
+++ README.md
@@ -0,0 +1,23 @@
+# TSLA Exchange
+
+Front end application for tsla.exchange.
+
+## Development
+
+Install dependencies:
+
+```bash
+yarn install
+```
+
+Run the development server:
+
+```bash
+yarn run nuxt
+```
+
+Generate static site:
+
+```bash
+yarn run nuxt generate
+```
---
+++ pages/index.vue
@@ -0,0 +1,160 @@
+<template>
+  <body>
+    <div class="v1_2">
+      <div class="v3_26" />
+      <div class="v3_28" />
+
+      <span class="v3_22">Get to the Moon
+        with your sTSLA</span>
+      <div class="v3_23" />
+
+      <span class="v3_24">Get sTSLA</span>
+
+      <span class="v3_25">Synthetix is the backbone for derivatives
+        trading in DeFi, allowing anyone, anywhere
+        to gain on-chain exposure to a vast range
+        of assets.
+      </span>
+
+      <div class="v3_27" />
+      <div class="v14_3" />
+      <div class="v14_4" />
+      <div class="v14_5" />
+    </div>
+  </body>
+</template>
+
+<style>
+* {
+  overflow-x: hidden;
+  padding: 0px !important;
+  margin: 0px !important;
+}
+.v1_2 {
+  width: 100%;
+  height: 1024px;
+  background: linear-gradient(rgba(0,2,75,1), rgba(39,30,110,1));
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v3_26 {
+  width: 100%;
+  height: 100%;
+  background: url("../public/v3_26.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 162px;
+  overflow: hidden;
+}
+.v3_28 {
+  width: 318px;
+  height: 155px;
+  background: url("../public/v3_28.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 580px;
+  left: 278px;
+  transform: rotate(-32deg);
+  overflow: hidden;
+}
+.v3_22 {
+  width: 470px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 134px;
+  left: 70px;
+  font-family: Roboto Condensed;
+  font-weight: Bold;
+  font-size: 64px;
+  opacity: 1;
+  text-align: left;
+}
+.v3_23 {
+  width: 186px;
+  height: 48px;
+  background: linear-gradient(rgba(130,68,176,1), rgba(255,0,119,1));
+  opacity: 1;
+  position: absolute;
+  top: 571px;
+  left: 70px;
+  border-top-left-radius: 50px;
+  border-top-right-radius: 50px;
+  border-bottom-left-radius: 50px;
+  border-bottom-right-radius: 50px;
+  overflow: hidden;
+}
+.v3_24 {
+  width: 125px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 581px;
+  left: 101px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v3_25 {
+  width: 519px;
+  color: rgba(147,159,189,1);
+  position: absolute;
+  top: 360px;
+  left: 70px;
+  font-family: Montserrat;
+  font-weight: Regular;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v3_27 {
+  width: 523px;
+  height: 297px;
+  background: url("../public/v3_27.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 740px;
+  left: 331px;
+  overflow: hidden;
+}
+.v14_3 {
+  width: 38px;
+  height: 37px;
+  background: rgba(255,255,255,1);
+  opacity: 1;
+  position: absolute;
+  top: 809px;
+  left: 70px;
+}
+.v14_4 {
+  width: 41px;
+  height: 34px;
+  background: rgba(255,255,255,1);
+  opacity: 1;
+  position: absolute;
+  top: 873px;
+  left: 70px;
+}
+.v14_5 {
+  width: 23px;
+  height: 36px;
+  background: rgba(255,255,255,1);
+  opacity: 1;
+  position: absolute;
+  top: 934px;
+  left: 77px;
+}
+</style>
